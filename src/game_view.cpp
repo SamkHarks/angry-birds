dad5b99246858c48961ef4960585cd1317cfc4bf
SamkHarks <samikh90@gmail.com>
2024-05-08 22:54:47 +0300

    remove member & use inheritance, and add base for draw method

--- src/game_view.cpp
+++ src/game_view.cpp
@@ -1,18 +1,25 @@
 #include "game_view.hpp"
 
-GameView::GameView() : window_(sf::VideoMode(1024, 720), "Angry Birds") {}
+GameView::GameView() : sf::RenderWindow(sf::VideoMode(1024, 720), "Angry Birds") {}
 
 
-void GameView::render() {
-    window_.clear(sf::Color::Blue);
-    draw();
-    window_.display();
+void GameView::render(const GameModel::State& state) {
+    this->clear(sf::Color::Blue);
+    draw(state);
+    this->display();
 }
 
-void GameView::draw() {
+void GameView::draw(const GameModel::State& state) {
     // TODO: Draw game objects here
-}
-
-sf::RenderWindow& GameView::getWindow() {
-    return window_;
+    switch (state) {
+        case GameModel::State::MENU:
+            // TODO: Draw menu
+            break;
+        case GameModel::State::RUNNING:
+            // TODO: Draw game objects
+            break;
+        case GameModel::State::PAUSED:
+            // TODO: Draw pause menu
+            break;
+    }
 }
--- src/game_view.hpp
+++ src/game_view.hpp
@@ -2,15 +2,13 @@
 #define GAME_VIEW_HPP
 
 #include <SFML/Graphics.hpp>
+#include "game_model.hpp"
 
-class GameView {
+class GameView: public sf::RenderWindow {
     public:
         GameView();
-        void draw();
-        void render();
-        sf::RenderWindow& getWindow();
-    private:
-        sf::RenderWindow window_;
+        void draw(const GameModel::State& state);
+        void render(const GameModel::State& state);
 };
 
 #endif // GAME_VIEW_HPP
