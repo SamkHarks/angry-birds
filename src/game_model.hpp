fe33488b58c69db37e42b82f8d92774d730e9f79
SamkHarks <samikh90@gmail.com>
2024-05-09 10:32:41 +0300

    add more states

--- src/game_model.hpp
+++ src/game_model.hpp
@@ -7,9 +7,14 @@ class GameModel {
         void update();
 
         enum class State {
-            MENU,
-            RUNNING,
-            PAUSED,
+            MENU,       // The game is at the main menu.
+            RUNNING,    // The game is currently being played.
+            PAUSED,     // The game is paused.
+            LOADING,    // The game is loading resources or a new level.
+            SETTINGS,   // The player is adjusting settings.
+            GAME_OVER,  // The game has ended.
+            WIN,        // The player has won the game.
+            LOSE,       // The player has lost the game.
         };
 
         const State& getState() const;
