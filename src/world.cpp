166f02a61887aa94238a348fa6448e85da66ae46
SamkHarks <samikh90@gmail.com>
2024-06-03 17:13:30 +0300

    add more methods to world class for creating objects

--- src/world.cpp
+++ src/world.cpp
@@ -13,7 +13,11 @@ World::~World() {
 }
 
 void World::addObject(Object *object) {
-    objects_.push_back(object);
+    if (object->getType() == Object::Type::Bird) {
+        birds_.push_back(dynamic_cast<Bird *>(object));
+    } else {
+        objects_.push_back(object);
+    }
 }
 
 void World::loadLevel(const std::string& filename) {
@@ -22,7 +26,19 @@ void World::loadLevel(const std::string& filename) {
     if(!file.is_open()) {
         throw std::runtime_error("Failed to open file: " + path + filename);
     }
+    // Read and set level name from the first line
     setLevelName(file);
+    // Read bird list from the second line
+    std::vector<Bird::Type> birdList = readBirdList(file);
+    // Read and create objects from the rest of the file
+    while (!file.eof()) {
+        ObjectData data = readObjectData(file);
+        b2Body *body = createBody(data);
+        b2FixtureDef fixtureDef;
+        readFixture(file, fixtureDef);
+        createObject(data.type, birdList, body, fixtureDef);
+        file.get(); // Read the newline character
+    }
 }
 
 void World::setLevelName(std::ifstream &file) {
@@ -33,13 +49,67 @@ void World::setLevelName(std::ifstream &file) {
     }
 }
 
+std::vector<Bird::Type> World::readBirdList(std::ifstream &file) {
+    std::vector<Bird::Type> birdList;
+    char birdType;
+    std::string birdListStr;
+    std::getline(file, birdListStr);
+    for (auto birdType : birdListStr) {
+        switch (birdType) {
+            case 'R':
+                birdList.push_back(Bird::Type::Red);
+                break;
+            case 'B':
+                birdList.push_back(Bird::Type::Blue);
+                break;
+            case 'G':
+                birdList.push_back(Bird::Type::Green);
+                break;
+            default:
+                throw std::runtime_error("Invalid bird type");
+        }
+    }
+    return birdList;
+}
+
 ObjectData World::readObjectData(std::ifstream &file) const {
+    char _; // Character to ignore
     char type;
     file.get(type);
     file.ignore();
     std::cout<<type<<std::endl;
     ObjectData data;
-    return data; // Remove this line and add your implementation here
+    // Read the object type from the file
+    switch (type) {
+        case 'B':
+            data.type = Object::Type::Bird;
+            break;
+        case 'G':
+            data.type = Object::Type::Ground;
+            break;
+        case 'P':
+            data.type = Object::Type::Pig;
+            break;
+        case 'W':
+            data.type = Object::Type::Wall;
+            break;
+        default:
+            throw std::runtime_error("Invalid object type");
+            break;
+    }
+
+    // Read ObjectData fields from the file
+    file>>data.position>>_
+    >>data.angle>>_
+    >>data.angularVelocity>>_
+    >>data.linearVelocity>>_
+    >>data.angularDamping>>_
+    >>data.linearDamping>>_
+    >>data.gravityScale>>_
+    >>data.bodyType>>_
+    >>data.awake>>_;
+    
+    return data;
 }
 
 b2Body* World::createBody(const ObjectData& data) {
@@ -51,7 +121,7 @@ b2Body* World::createBody(const ObjectData& data) {
     body_def.angularDamping = data.angularDamping;
     body_def.linearDamping = data.linearDamping;
     body_def.gravityScale = data.gravityScale;
-    body_def.type = static_cast<b2BodyType>(data.bodyType);
+    body_def.type = data.bodyType;
     body_def.awake = data.awake;
 
     b2Body *body = world_->CreateBody(&body_def);
@@ -59,8 +129,77 @@ b2Body* World::createBody(const ObjectData& data) {
     return body;
 }
 
+void World::createShape(int shapeType, std::stringstream& fixture, b2FixtureDef& fixtureDef) {
+    char _; // Character to ignore
+    switch (shapeType) {
+        case b2Shape::Type::e_circle: {
+            b2CircleShape* circle = new b2CircleShape;
+            fixture >> circle->m_p >> _ >> circle->m_radius >> _;
+            fixtureDef.shape = circle;
+            break;
+        }
+        case b2Shape::Type::e_polygon:
+            //TODO: Implement polygon shape
+            break;
+        default:
+            throw std::runtime_error("Unknown shape type");
+    }
+}
+
+void World::readFixture(std::ifstream& file, b2FixtureDef& fixtureDef) {
+    char _; // Character to ignore
+    std::string fixtureStr;
+    std::getline(file, fixtureStr, '\n');
+    std::stringstream fixture(fixtureStr);
+    int shapeType;
+    fixture >> shapeType >> _;
+    createShape(shapeType, fixture, fixtureDef);
+    fixture >> fixtureDef.density >> _ >> fixtureDef.friction >> _ >> fixtureDef.restitution >> _;
+}
+
+void World::createObject(
+    Object::Type objType,
+    std::vector<Bird::Type> birdList,
+    b2Body* body,
+    b2FixtureDef& fixtureDef
+) {
+    //Object *object = nullptr;
+    switch (objType) {
+        case Object::Type::Bird:
+            for (auto birdType : birdList) {
+                Bird *bird;
+                switch (birdType) {
+                    case Bird::Type::Red:
+                        bird = new RedBird(body);
+                        break;
+                    case Bird::Type::Blue:
+                        // TODO: Implement Yellow bird
+                        break;
+                    case Bird::Type::Green:
+                        // TODO: Implement Green bird
+                        break;
+                }
+                addObject(bird);
+                fixtureDef.userData.pointer = reinterpret_cast<uintptr_t>(bird);
+            }
+            break;
+        case Object::Type::Ground:
+            //TODO: Implement Ground object
+            break;
+        case Object::Type::Pig:
+            //TODO: Implement Pig object
+            break;
+        case Object::Type::Wall:
+            //TODO: Implement Wall object
+            break;
+        default:
+            throw std::runtime_error("Invalid object type");
+    }
+}
+
+
 void World::step() {
-    world_->Step(1.0f / 60.0f, 8, 3);
+    world_->Step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
 }
 
 b2World* World::getWorld() {
--- src/world.hpp
+++ src/world.hpp
@@ -5,6 +5,11 @@
 #include <list>
 #include "bird.hpp"
 
+const int FRAME_RATE = 60;
+const float TIME_STEP = 1.0f / FRAME_RATE;
+const int VELOCITY_ITERATIONS = 8;
+const int POSITION_ITERATIONS = 3;
+
 struct ObjectData {
     Object::Type type; // Object type
     b2Vec2 position; // Position of the object
@@ -14,7 +19,7 @@ struct ObjectData {
     float angularDamping; // Angular damping of the object
     float linearDamping; // Linear damping of the object
     float gravityScale; // Gravity scale of the object
-    int bodyType; // Body type of the object, b2_staticBody, b2_kinematicBody, b2_dynamicBody
+    b2BodyType bodyType; // Body type of the object, b2_staticBody, b2_kinematicBody, b2_dynamicBody
     bool awake; // Whether the object is awake
 
     // Add more fields here as needed...
@@ -26,8 +31,12 @@ class World {
         ~World();
         void loadLevel(const std::string& filename);
         void setLevelName(std::ifstream &file);
+        std::vector<Bird::Type> readBirdList(std::ifstream &file); 
         ObjectData readObjectData(std::ifstream &file) const;
-        b2Body* createBody(const ObjectData& data); 
+        b2Body* createBody(const ObjectData& data);
+        void readFixture(std::ifstream& file, b2FixtureDef& fixture_def);
+        void createShape(int shape_type, std::stringstream& fixture, b2FixtureDef& fixtureDef);
+        void createObject(Object::Type objType, std::vector<Bird::Type> birdList, b2Body* body, b2FixtureDef& fixtureDef);
         void addObject(Object *object);
         void step();
         b2World* getWorld();
