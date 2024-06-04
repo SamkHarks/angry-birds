4bd12f8b3daadd31becc8a4ad213efad3d96fdcc
SamkHarks <samikh90@gmail.com>
2024-06-04 22:39:53 +0300

    load level

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,10 @@
 #include "game.hpp"
+#include "world.hpp"
 
 Game::Game() : model_(), view_(), controller_(model_, view_) {}
 
 void Game::run() {
+    model_.getWorld().loadLevel("level1.txt");
     while (view_.isOpen()) {
         controller_.handleEvents();
         model_.update();
