f54c1b97dee26ee196ca23d3be818ecc481f0eb1
SamkHarks <samikh90@gmail.com>
2024-06-23 12:56:25 +0300

    add todo and method def

--- src/wall.cpp
+++ src/wall.cpp
@@ -2,10 +2,9 @@
 #include "utils.hpp"
 
 Wall::Wall(b2Body *body) : Object(body, Type::Wall, "/assets/box.png") {
-    int width = sprite_.getTextureRect().width;
-    int height = sprite_.getTextureRect().height;
-    sprite_.setScale(1.0f * SCALE / (static_cast<float>(width)), 1.0f * SCALE / (static_cast<float>(height)));
-    sprite_.setOrigin(width / 2, height / 2);
-    sf::Vector2 sfPosition = utils::B2ToSfCoords(body_->GetPosition());
-    sprite_.setPosition(sfPosition.x, sfPosition.y);
+    //TODO: Implement this
+}
+
+char Wall::getTypeAsChar() const {
+    return 'W';
 }
