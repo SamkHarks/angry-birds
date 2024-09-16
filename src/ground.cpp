cbc258c13b17bd7d16399d570e5ed16f340ea892
SamkHarks <samikh90@gmail.com>
2024-09-16 22:56:28 +0300

    update ground initialization

--- src/ground.cpp
+++ src/ground.cpp
@@ -5,13 +5,13 @@
 Ground::Ground(b2Body *body, float hx, float hy) : Object(body, Type::Ground, "/assets/images/ground.png") {
     float width = static_cast<float>(sprite_.getTextureRect().width);
     float height = static_cast<float>(sprite_.getTextureRect().height);
-    float heightY = 2.f * hy * SCALE;
-    float scaleX = (2.f * hx * SCALE) / width;
-    float scaleY = (heightY + 30) / height;
+    float heightSf = utils::B2ToSf(2.f * hy);
+    float scaleX = utils::B2ToSf(2.f * hx) / width;
+    float scaleY = heightSf / height;
     sprite_.setScale(scaleX, scaleY);
     texture_.setRepeated(true);
     sf::Vector2f centerPosition = utils::B2ToSfCoords(body->GetPosition());
-    sprite_.setPosition(0, centerPosition.y - (heightY - 20));
+    sprite_.setPosition(0, centerPosition.y - heightSf);
 }
 
 char Ground::getTypeAsChar() const {
