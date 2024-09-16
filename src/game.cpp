63db639e98bfd05d3bae5d434eb74d10891fcc2d
SamkHarks <samikh90@gmail.com>
2024-09-16 09:29:18 +0300

    add todo

--- src/game.cpp
+++ src/game.cpp
@@ -1,10 +1,12 @@
 #include "game.hpp"
 #include "world.hpp"
+#include "sfml_debug_draw.hpp"
 
 Game::Game() : model_(), view_(), controller_(model_, view_) {}
 
 void Game::run() {
     model_.getWorld().loadLevel("level1.txt");
+    // TODO: Add possibility to load debug draw
     while (view_.isOpen()) {
         controller_.handleEvents();
         model_.update();
