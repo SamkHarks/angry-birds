e38537263d1430a2ff3b053fbbead180a2e6d3b5
SamkHarks <samikh90@gmail.com>
2024-09-23 20:08:33 +0300

    add game over menu class

---
+++ src/game_over.cpp
@@ -0,0 +1,19 @@
+#include "game_over.hpp"
+#include "utils.hpp"
+
+GameOver::GameOver() : Menu(Menu::Type::GAME_OVER, 4) {
+    std::string button_texts[buttonAmount_] = { "Restart", "Next Level", "Main Menu", "Exit" };
+    for (int i = 0; i < buttonAmount_; ++i) {
+        int itemWidth = button_texts[i].length() * 80;
+        float x = (VIEW_WIDTH - itemWidth) / 2;
+        menuItems_[i].setFont(font_);
+        menuItems_[i].setString(button_texts[i]);
+        menuItems_[i].setFillColor(sf::Color::Black);
+        menuItems_[i].setCharacterSize(80);
+        menuItems_[i].setPosition(x , 200 + i * 100);
+        if (i == selectedItem_) {
+            menuItems_[i].setOutlineColor(sf::Color::White);
+            menuItems_[i].setOutlineThickness(5);
+        }
+    }
+}
---
+++ src/game_over.hpp
@@ -0,0 +1,11 @@
+#ifndef GAME_OVER_HPP
+#define GAME_OVER_HPP
+
+#include "menu.hpp"
+
+class GameOver: public Menu {
+    public:
+        GameOver();
+};
+
+#endif // GAME_OVER_HPP
