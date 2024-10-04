dd0be7964e3e48895b64c90c3f08e7532993a12d
SamkHarks <samikh90@gmail.com>
2024-10-04 23:46:17 +0300

    add score to game over menu and center menu items

--- src/game_over.cpp
+++ src/game_over.cpp
@@ -5,16 +5,31 @@
 GameOver::GameOver() : Menu(Menu::Type::GAME_OVER, 4) {
     std::vector<std::string> button_texts = { "Restart", "Next Level", "Main Menu", "Exit" };
     for (int i = 0; i < buttonAmount_; ++i) {
-        int itemWidth = button_texts[i].length() * 80;
-        float x = (VIEW_WIDTH - itemWidth) / 2;
         menuItems_[i].setFont(font_);
         menuItems_[i].setString(button_texts[i]);
         menuItems_[i].setFillColor(sf::Color::Black);
         menuItems_[i].setCharacterSize(80);
-        menuItems_[i].setPosition(x , 200 + i * 100);
+        // center the text
+        sf::FloatRect textBounds = menuItems_[i].getGlobalBounds();
+        menuItems_[i].setOrigin(textBounds.width / 2, textBounds.height / 2);
+        menuItems_[i].setPosition(SCREEN_CENTER.x , 200 + i * 100);
         if (i == selectedItem_) {
             menuItems_[i].setOutlineColor(sf::Color::White);
             menuItems_[i].setOutlineThickness(5);
         }
     }
 }
+
+void GameOver::setScoreManager(Score* scoreManager) {
+    scoreManager_ = scoreManager;
+    scoreManager_->setPosition(sf::Vector2f(SCREEN_CENTER.x, 100));
+}
+
+void GameOver::draw(sf::RenderWindow& window) const {
+    window.draw(background_);
+    scoreManager_->draw(window);
+    for (auto menuItem : menuItems_)
+    {
+        window.draw(menuItem);
+    }
+};
--- src/game_over.hpp
+++ src/game_over.hpp
@@ -2,10 +2,15 @@
 #define GAME_OVER_HPP
 
 #include "menu.hpp"
+#include "score.hpp"
 
 class GameOver: public Menu {
     public:
         GameOver();
+        void setScoreManager(Score* score);
+        void draw(sf::RenderWindow& window) const override;
+    private:
+       Score* scoreManager_;
 };
 
 #endif // GAME_OVER_HPP
