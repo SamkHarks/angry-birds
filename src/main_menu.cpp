20e57de7c8dd1b8d03b3ba820eb9af7bcf2746ec
SamkHarks <samikh90@gmail.com>
2024-10-04 23:50:16 +0300

    center main menu items

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -1,5 +1,6 @@
 #include "main_menu.hpp"
 #include <vector>
+#include "utils.hpp"
 
 
 MainMenu::MainMenu() : Menu(Menu::Type::MAIN, 3) {
@@ -9,7 +10,10 @@ MainMenu::MainMenu() : Menu(Menu::Type::MAIN, 3) {
         menuItems_[i].setString(button_texts[i]);
         menuItems_[i].setFillColor(sf::Color::Black);
         menuItems_[i].setCharacterSize(80);
-        menuItems_[i].setPosition(400, 200 + i * 100);
+        // center the text
+        sf::FloatRect textBounds = menuItems_[i].getGlobalBounds();
+        menuItems_[i].setOrigin(textBounds.width / 2, textBounds.height / 2);
+        menuItems_[i].setPosition(SCREEN_CENTER.x, 300 + i * 100);
         if (i == selectedItem_) {
             menuItems_[i].setOutlineColor(sf::Color::White);
             menuItems_[i].setOutlineThickness(5);
